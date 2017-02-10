2ca082ce5194587d3bbac8951a8272d8e636ad7b
Frank Polte <frank.polte@gmail.com>
2017-02-10 22:33:27 +0100

    add new resource

--- src/db.py
+++ src/db.py
@@ -6,7 +6,10 @@ import tinydb
 import sys
 import os
 from configuration import resources as resourceConfig
+import inspect
+import schema
 
+# get the db file path
 defaultKaranaDbPath = "~/karana_db.json"
 __karanaDbPath__ = ""
 
@@ -25,6 +28,11 @@ if 'KARANA_BACKEND_DB' in os.environ.keys():
 else:
      __karanaDbPath__  = defaultKaranaDbPath
 
+# all defined schemas in a dict
+globalschemas = {}
+for name, obj in inspect.getmembers(schema):
+    globalschemas[name]=obj()
+
 class KaranaDBWrapper(object):
     ''' Karana DB Wrapper'''
 
@@ -32,29 +40,43 @@ class KaranaDBWrapper(object):
         self.db = tinydb.TinyDB(__karanaDbPath__)
         self.res_schema = None
         self.tables = {}
-        self.__uuid_index__ = {}
+        self.uuid_index = {}
+        self.schema_index = {}
+        self.__prepare_db__()
 
+    def __prepare_db__(self):
         # sanitatized import, consistency checks and repair/migration of old databases needed
-        for res_id in resourceConfig.keys():
-            self.tables[resourceConfig[res_id]['name']] = self.db.table(resourceConfig[res_id]['name'])
-            self.tables[resourceConfig[res_id]['name']].insert(resourceConfig[res_id]['metadata'])
+        # resourceConfig[res_id]['name'] need to fit in a schema (a-z,A-Z,0-9)
+        for res_table_id in resourceConfig.keys():
+            log.debug('"create table instances (no overwrite)"')
+            res_table_name = resourceConfig[res_table_id]['name']
+            self.tables[res_table_name] = self.db.table(res_table_name)
+            log.debug('"overwrite metadata entry in db with the one from configuration"')
+            self.tables[res_table_name].insert(resourceConfig[res_table_name]['metadata'])
+            schema_name = resourceConfig[res_table_name]['entry_schema']
+            if schema_name in globalschemas.keys():
+                self.schema_index[res_table_name] = globalschemas[schema_name]
 
     def update_uuid_index(self):
         try:
             for table in self.tables:
                 for entry in table.all()
                     if 'uuid' in entry.keys():
-                        self.__uuid_index__[entry["uuid"]] = table[entry["uuid"]]
+                        self.uuid_index[entry["uuid"]] = table[entry["uuid"]]
         except:
             log.error("could not update uuid index, maybe some tables or entries are broken")
 
-    def add_new_res(self, table, res):
-        pass
+    def add_new_res(self, table: str, res: str):
+        try:
+            new_res = False
+            new_res = self.schema_index[table].loads(res)
+            if not new_res.errors:
+                self.tables[table].insert(new_res.data)
+        except:
+            log.error("resource json validation or db import error")
+
 
     def modify_value_in_res(self, res, key, value):
-        pass
-
-    def get_res_tables(self):
         pass
 
     def get_table_schema(self):
