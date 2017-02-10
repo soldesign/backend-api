3709c6215ac00003604ee5df95e75fa2aa6908c1
micha <michael.goette@microenergy-international.com>
2017-02-10 17:42:27 +0100

    added log file

---
+++ src/log.py
@@ -0,0 +1,7 @@
+import logging
+
+
+
+FORMAT = 'karana backend api, log %(levelname)s: %(message)s'
+log = logging
+logging.basicConfig(level=0, format=FORMAT)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-##
+#!/usr/bin/env python3
+
+docstring='''  '''
+
+
+
+import hug
+from log import log
+
+log.info(docstring)
+
