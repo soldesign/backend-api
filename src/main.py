577a0ab2106414b579be1500a8e2bdc478a390a0
micha <michael.goette@microenergy-international.com>
2017-02-10 18:54:10 +0100

    main for testing

--- src/main.py
+++ src/main.py
@@ -8,27 +8,32 @@ from log import log
 log.info(docstring)
 
 
-@hug.get('{resources}/{resources_id}', version=1)
-def get_resource():
+@hug.get('/api/{resources}/{resource_id}', version=1)
+def get_resource(resources: hug.types.text, resource_id: hug.types.text):
     """This method returns either the resource with given ID or al resources"""
+    return True
 
 
-@hug.post('{resources}/new', version=1)
-def create_resource():
+@hug.post('/api/{resources}/new', version=1)
+def create_resource(resources: hug.types.text, body):
     """This method creates a resource"""
+    return True
 
 
-@hug.put('{resources}/{resource_id}', version=1)
-def updated_resource():
+@hug.put('/api/{resources}/{resource_id}/', version=1)
+def updated_resource(resources: hug.types.text, resource_id: hug.types.text, body):
     """This method updates a resource completely"""
+    return True
 
 
-@hug.patch('{resources}/{resource_id}', version=1)
-def modify_resource():
+@hug.patch('/api/{resources}/{resource_id}/', version=1)
+def modify_resource(resources: hug.types.text, resource_id: hug.types.text, body):
     """"This method modifies a resource in this case only one field allowed"""
+    return True
 
 
-@hug.delete('{resources}/{resource_id}', version=1)
-def delete_resource():
+@hug.delete('/api/{resources}/{resource_id}/', version=1)
+def delete_resource(resources: hug.types.text, resource_id: hug.types.text):
     """This method deletes a resource"""
+    return True
 
