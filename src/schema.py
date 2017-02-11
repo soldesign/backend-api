d0eb795acdb6baa0d1a50c76d50b12eb3fd5e4f3
micha <michael.goette@microenergy-international.com>
2017-02-11 13:41:19 +0100

    added test suit for py.test

--- src/schema.py
+++ src/schema.py
@@ -1,7 +1,13 @@
 '''This file contains the short Version of the two main schemas User and Karanas'''
 import datetime as dt
 import uuid
+import random
+import string
 from marshmallow import Schema, fields, post_load
+
+
+def id_generator(size=16, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 ##################################### MODELS ####################################################
@@ -11,6 +17,7 @@ class User(object):
     def __init__(self, name, email, role):
         self.uuid = uuid.uuid4()
         self.name = name
+        self.credentials = {"password": id_generator()}
         self.email = email
         self.created_at = dt.datetime.now()
         self.role = role
@@ -21,6 +28,7 @@ class Karana(object):
     def __init__(self, name, note, user):
         self.uuid = uuid.uuid4()
         self.name = name
+        self.credentials = {"password": id_generator()}
         self.note = note
         self.created_at = dt.datetime.now()
         self.user = user
@@ -32,6 +40,7 @@ class UserSchema(Schema):
     uuid = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     email = fields.Email(required=True)
+    credentials = fields.Dict()
     created_at = fields.DateTime(dump_only=True)
     role = fields.Str(required=True)
     karanas = fields.Nested('KaranaSchema', many=True, exclude=('user', 'note', 'created_at'))
@@ -45,6 +54,7 @@ class KaranaSchema(Schema):
     uuid = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     note = fields.Str()
+    credentials = fields.Dict()
     created_at = fields.DateTime(dump_only=True)
     user = fields.Nested('UserSchema', only=["uuid", "email"], required=True)
 
