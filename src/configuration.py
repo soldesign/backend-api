a709d4c4fcdaec379175bf622f2c368b2435cad7
Frank Polte <frank.polte@gmail.com>
2017-02-16 20:04:12 +0100

    configuration.py: api_metadata added

--- src/configuration.py
+++ src/configuration.py
@@ -1,8 +1,11 @@
 
 ''' this file contains the configuration'''
 
+################################################################################
+## resource definition section
+################################################################################
 
-
+####### single resources (take care with the structure)
 users = {'metadata': {'res_table_id': 1,\
                       'schema': {'entry_create_schema': "UserSchema",\
                                  'entry_import_schema': "UserDbSchema"
@@ -22,11 +25,33 @@ karanas = {'metadata': {'res_table_id': 2,\
           }
 
 
+######## the collection of used resources (only this structure is directly used)
 resources = { users['metadata']['res_table_id']: users,\
               karanas['metadata']['res_table_id']: karanas\
               }
+################################################################################
+################################################################################
 
-loginresource = 'users'
+################################################################################
+## api instance general metadata + configuration
+################################################################################
 
-# maybe used later
-index = {}
+api_metadata = {'tenant_id': 'SMNTYQIUB4YTC',
+                'tenant_customer_name': 'Bintumani e.V.',
+                'tenant_login_credentials_path': ['users', 'credentials'], # path to the login credentials (define in Schema: here 'UserSchema')
+                'tenant_used_login_credentials': ['login', 'pwhash']
+               }
+
+
+
+
+'''
+##########################
+#### example tenant_id generator dunction
+import base64
+import random
+
+keysize = 64
+example_id = base64.b32encode(random.getrandbits(keysize).to_bytes(int(keysize/8), "big"))[:-3]
+##########################
+'''
